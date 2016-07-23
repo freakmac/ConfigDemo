e2ef94eeff3ddab520b6c9df6b4ddd9308f8a078
Charles J Cromer <cjc4@alumni.duke.edu>
2016-07-23 04:50:05 -0400

    Initial Commit

---
+++ ConfigDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConfigDemo
+//
+//  Created by Jeremy Cromer on 7/23/16.
+//  Copyright © 2016 Jeremy Cromer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConfigDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConfigDemo
+//
+//  Created by Jeremy Cromer on 7/23/16.
+//  Copyright © 2016 Jeremy Cromer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
