6eeddac213bf25a361e73913ac93bbac7a443251
Charles J Cromer <cjc4@alumni.duke.edu>
2016-07-23 04:57:07 -0400

    insert println message

--- ConfigDemo/ViewController.m
+++ ConfigDemo/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    println("I can't wait to push this project to Github");
 }
 
 - (void)didReceiveMemoryWarning {
